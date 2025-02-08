f44471c951b2798a0c429c054f2419f56a1b44df
A6ih <177154114+A6ih@users.noreply.github.com>
2025-02-08 13:53:20 +0200

    Add function and event listeners to show dialog and edit the task

--- src/dom.js
+++ src/dom.js
@@ -4,10 +4,14 @@ const projectDialog = document.querySelector("#project-dialog");
 const projectNav = document.querySelector("#project-nav");
 const taskDialog = document.querySelector("#task-dialog");
 const selectProject = document.querySelector("#select-project");
+const editTaskDialog = document.querySelector("#edit-task-dialog");
 let deleteProjectBtns;
 let projectTitleClick;
 let deleteTaskBtns;
 let taskExpandBtn;
+let editTaskBtn;
+let projectId;
+let taskId;
 
 export function eventListeners(addProjectFn, deleteProjectFn, addTaskFn, deleteTaskFn, arr, editTaskFn){
 
@@ -55,7 +59,26 @@ export function eventListeners(addProjectFn, deleteProjectFn, addTaskFn, deleteT
         clearInputs( [title, description, dueDate, note] );
         document.querySelector("#medium").checked = "true";
         taskDialog.close();
+    })
 
+    document.querySelector("#save-task-btn").addEventListener("click", function(event) {
+        event.preventDefault();
+        const title = document.querySelector("#edit-task-title");
+        const description = document.querySelector("#edit-task-description");
+        const dueDate = document.querySelector("#edit-task-due-date");
+        const priority = document.querySelector("input[name='edit-priority']:checked");
+        const note = document.querySelector("#edit-task-note");
+
+        editTaskFn(projectId, taskId, title.value, description.value, dueDate.value, priority.value, note.value);
+        renderTasks(arr[projectId], updateTaskElements);
+        clearInputs([title, description, dueDate, note]);
+        document.querySelector("#edit-medium").checked = "true";
+        editTaskDialog.close();
+    })
+
+    document.querySelector("#edit-task-cancel-btn").addEventListener("click", function(event) {
+        event.preventDefault();
+        editTaskDialog.close();
     })
 
     renderProjectNav(arr, updateProjectElements);
@@ -82,7 +105,7 @@ export function eventListeners(addProjectFn, deleteProjectFn, addTaskFn, deleteT
 
     function deleteTask(event) {
         const projectIndex = +event.target.dataset.taskDeleteProject;
-        const index = +event.target.dataset.taskDelete;
+        const index = +event.target.dataset.taskDelete
         deleteTaskFn(projectIndex, index);
         renderTasks(arr[projectIndex], updateTaskElements);
     }
@@ -90,8 +113,10 @@ export function eventListeners(addProjectFn, deleteProjectFn, addTaskFn, deleteT
     function updateTaskElements() {
         deleteTaskBtns = Array.from(document.querySelectorAll(".task-delete-btn"));
         taskExpandBtn = Array.from(document.querySelectorAll(".task-expand-btn"));
+        editTaskBtn = Array.from(document.querySelectorAll(".task-edit-btn"));
         deleteTaskBtns.forEach(item => item.addEventListener("click", deleteTask));
         taskExpandBtn.forEach(item => item.addEventListener("click", taskToggleView));
+        editTaskBtn.forEach(item => item.addEventListener("click", showEditTaskDialog));
     }
 
     function taskToggleView(event) {
@@ -105,6 +130,12 @@ export function eventListeners(addProjectFn, deleteProjectFn, addTaskFn, deleteT
             hideTaskElements(index);
             event.target.textContent = "▼"
         }
+    }
+
+    function showEditTaskDialog(event) {
+        projectId = +event.target.dataset.taskEditProject;
+        taskId = +event.target.dataset.taskEdit;
+        editTaskDialog.showModal();
     }
 }
 
@@ -215,4 +246,20 @@ function hideTaskElements(index) {
     description[index].style.display = "none";
     notes[index].style.display = "none";
     BtnContainer[index].style.display = "none";
+}
+
+function setProjectId(index) {
+    projectId = index;
+}
+
+function setTaskId(index)  {
+    taskId = index;
+}
+
+function getProjectId() {
+    return projectId;
+}
+
+function getTaskId() {
+    return taskId;
 }
