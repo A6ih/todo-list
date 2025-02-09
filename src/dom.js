2131c5b1171834893ef400f5d7be7dbbb791740b
A6ih <177154114+A6ih@users.noreply.github.com>
2025-02-10 00:44:26 +0200

    Add domHelpers.js file and moved some functions from dom.js to domHelpers

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,6 @@
 import createElement from "./createElement";
-import { format, isToday, isTomorrow, isPast, isYesterday} from "date-fns";
+import { format } from "date-fns";
+import { clearInputs, hideAllTaskElements, showTaskElements, hideTaskElements, updateRenderDate, addPriorityColor } from "./domHelpers";
 
 const projectDialog = document.querySelector("#project-dialog");
 const projectNav = document.querySelector("#project-nav");
@@ -245,68 +246,7 @@ function renderProjectOptions(arr) {
     }
 }
 
-function clearInputs(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        arr[i].value = "";
-    }
-}
-
-function hideAllTaskElements() {
-    Array.from(document.querySelectorAll(".task-description")).
-        forEach(item => item.style.display = "none");
-    Array.from(document.querySelectorAll(".task-note")).
-        forEach(item => item.style.display = "none");
-    Array.from(document.querySelectorAll(".task-btn-container")).
-        forEach(item => item.style.display = "none");
-}
-
-function showTaskElements(index) { 
-    const description = document.querySelectorAll(".task-description");
-    const notes = document.querySelectorAll(".task-note");
-    const BtnContainer = document.querySelectorAll(".task-btn-container");
-    description[index].style.display = "block";
-    notes[index].style.display = "block";
-    BtnContainer[index].style.display = "flex";
-}
-
-function hideTaskElements(index) {
-    const description = document.querySelectorAll(".task-description");
-    const notes = document.querySelectorAll(".task-note");
-    const BtnContainer = document.querySelectorAll(".task-btn-container");
-    description[index].style.display = "none";
-    notes[index].style.display = "none";
-    BtnContainer[index].style.display = "none";
-}
-
-function updateRenderDate(date) {
-    if (isToday(new Date(date))) {
-        return "Today";
-    }
-    else if(isTomorrow(new Date(date))) {
-        return "Tomorrow";
-    }
-    else if(isYesterday(new Date(date))) {
-        return "Expired Yesterday"
-    }
-    else if(isPast(new Date(date))) {
-        return `Expired on ${format(new Date(date), "dd MMMM yyyy")}`;
-    }
-    else {
-        return format(new Date(date), "dd MMMM yyyy");
-    }
-}
 
 function updateToday() {
     today = format(new Date(), "yyyy-LL-dd");
-}
-
-function addPriorityColor(div, element) {
-    if(element.textContent === "Priority: High") {
-        div.style.borderColor = "var(--red-color)";
-        element.style.textShadow = "0 0 2px var(--red-color)"
-    }
-    else if(element.textContent === "Priority: Medium") {
-        div.style.borderColor = "var(--yellow-color)";
-        element.style.textShadow = "0 0 2px var(--yellow-color)"
-    }
 }
